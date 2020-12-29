0f5f0cd2d76c86fada049d382a436f2d4a609bc1
gagdiez <gagdiez.c@gmail.com>
2020-12-29 14:51:01 +0100

    Added comments to better explain parameters

--- scripts/RedBox.gd
+++ scripts/RedBox.gd
@@ -3,6 +3,8 @@ extends Interactive
 func _ready():
 	# We can take this object
 	main_action = ACTIONS.take
+	
+	# The secondary_action is (implicitly) ACTIONS.examine
 
 	# We have to stand a couple of pixels away from it to interact
 	interaction_position = self.transform.origin + Vector3(3, 0, 0)
@@ -10,4 +12,3 @@ func _ready():
 	# Description and Thumbnail
 	description = "The box I have to move"
 	thumbnail = "res://thumbnails/red_box.png"
-
--- scripts/WhiteBox.gd
+++ scripts/WhiteBox.gd
@@ -1,7 +1,8 @@
 extends Interactive
 
 func _ready():
-	# For the WHITE box, the main_action is (implicitly) "walk_to"
+	# The main_action is (implicitly) "walk_to"
+	# The secondary_action is (implicitly) ACTIONS.examine
 	
 	# We have to stand a couple of pixels away from it to interact
 	interaction_position = self.transform.origin - Vector3(3, 0, 0)
