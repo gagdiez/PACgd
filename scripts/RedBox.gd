e1f9df5e59f502a913bf03ef379d6d318a573a8b
gagdiez <gagdiez.c@gmail.com>
2020-12-30 11:33:21 +0100

    Simplified code

--- scripts/RedBox.gd
+++ scripts/RedBox.gd
@@ -12,3 +12,25 @@ func _ready():
 	# Description and Thumbnail
 	description = "The box I have to move"
 	thumbnail = "res://thumbnails/red_box.png"
+
+func use_item(who, what):
+	# Since we only have one other element in the screen, <what> will be
+	# the WHITE box
+	
+	who.approach(what)
+	who.face_object(what)
+	who.say("Time to place one box on top of the other")
+	who.animate_until_finished("raise_hand")
+	who.remove_from_inventory(self)
+	who.interact(self, "place_over", [what])
+	who.animate_until_finished("lower_hand")
+	who.say("That is it, thank you for playing")
+
+func place_over(box):
+	# Function called by who.interact(self, "place_box")
+	# Place the box on top of the WHITE one
+	self.transform.origin = box.transform.origin + Vector3(0, 2, 0)
+	self.visible = true
+	
+	# Disable interactions with the white box
+	box.interactive = false
