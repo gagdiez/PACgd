b8c6f3bf395bcf9e682061b44f60f79ee31dc8a5
LateNightIceCream <richardgruenert@live.com>
2023-05-16 17:16:54 +0200

    Fix Camera node reference in example level
    There is no node called "Camera3D" in the scene tree. It should be "Camera".

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -7,7 +7,7 @@ func _ready():
 
 	# - The Camera3D lets the player know which direction he should be facing when
 	#   moving around 
-	$Cole.camera = $Camera3D
+	$Cole.camera = $Camera
 	
 	# We need to initialize our point and click system letting it know:
 	# - Who's the player
